cec4324b4d408f503ef0da682234599c1ab23617
Ahmed Badawi <ahmedabadawi@gmail.com>
2016-11-17 04:52:27 +0200

    Override conventions for Gig and Genre EF migration

--- GigHub.Web/Data/Migrations/20161117025149_OverrideConventionsForGigsAndGenre.Designer.cs
+++ GigHub.Web/Data/Migrations/20161117025149_OverrideConventionsForGigsAndGenre.Designer.cs
@@ -8,9 +8,10 @@ using GigHub.Web.Data;
 namespace GigHub.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161117025149_OverrideConventionsForGigsAndGenre")]
+    partial class OverrideConventionsForGigsAndGenre
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
--- GigHub.Web/Models/Genre.cs
+++ GigHub.Web/Models/Genre.cs
@@ -1,5 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Genre
 {
     public byte Id { get; set; }
+    
+    [Required]
+    [StringLength(255)]
     public string Name { get; set; }
 }
