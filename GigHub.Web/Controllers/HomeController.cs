7a40f9f4d06fec97816187086f47bc173248ad3f
Ahmed Badawi <ahmedabadawi@gmail.com>
2016-11-21 15:10:59 +0200

    Implement upcoming gigs and improve UI of the home page

--- GigHub.Web/Controllers/HomeController.cs
+++ GigHub.Web/Controllers/HomeController.cs
@@ -2,29 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+
+using GigHub.Web.Data;
+using GigHub.Web.Models;
 
 namespace GigHub.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController (
+            ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            var upcomingGigs = 
+                _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .Where(g => g.DateTime > DateTime.Now)
+                .ToList();
+            
+            return View(upcomingGigs);
         }
 
         public IActionResult Error()
--- GigHub.Web/Models/ApplicationUser.cs
+++ GigHub.Web/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace GigHub.Web.Models
@@ -9,5 +10,8 @@ namespace GigHub.Web.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
     }
 }
