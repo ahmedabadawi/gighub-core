9d35c69f9411f165f56f45926384d380f331df85
Ahmed Badawi <ahmedabadawi@gmail.com>
2016-11-22 17:37:58 +0200

    Implemented Artists I'm Following

---
+++ GigHub.Web/Controllers/FolloweesController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+using GigHub.Web.ViewModels;
+using GigHub.Web.Data;
+using GigHub.Web.Models;
+
+namespace GigHub.Web.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
+
+        public FolloweesController (
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            ILoggerFactory loggerFactory)
+        {
+            _userManager = userManager;
+            _context = context;
+            _logger = loggerFactory.CreateLogger<GigsController>();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+            
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == currentUser.Id)
+                .Select(f => f.Followee)
+                .ToList();
+            
+            return View(artists);
+        }
+    }
+}
